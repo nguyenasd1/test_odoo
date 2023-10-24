c3534762189259f851f5bfd6174a4dd4b61c4840
nguyenasd1 <nguyenasd01@gmail.com>
2023-10-24 15:35:31 +0700

    Update employee.py

--- employee/models/employee.py
+++ employee/models/employee.py
@@ -11,6 +11,6 @@ class Employee(models.Model):
             raise AccessDenied("ok Manager")
         else:
             raise AccessDenied("ko co Manager???")
-        # heheheeheheh1
-# áadasdasdad
+
+
 
